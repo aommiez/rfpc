d63b47c45b64af014f983e81c110d4562d9ae020
MRg <aommiez@gmail.com>
2014-09-15 11:00:25 +0700

    init project

---
+++ Real Fish Prawn Crab/Real Fish Prawn Crab/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  Real Fish Prawn Crab
+//
+//  Created by issrapong wongyai on 9/15/2557 BE.
+//  Copyright (c) 2557 platwo fusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Real Fish Prawn Crab/Real Fish Prawn CrabTests/Real_Fish_Prawn_CrabTests.m
@@ -0,0 +1,34 @@
+//
+//  Real_Fish_Prawn_CrabTests.m
+//  Real Fish Prawn CrabTests
+//
+//  Created by issrapong wongyai on 9/15/2557 BE.
+//  Copyright (c) 2557 platwo fusion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Real_Fish_Prawn_CrabTests : XCTestCase
+
+@end
+
+@implementation Real_Fish_Prawn_CrabTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
